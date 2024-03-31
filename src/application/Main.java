d0d45ab93f06a4ef990b8fe9468c375956aba057
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2024-03-30 23:16:24 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private static Scene mainScene;
+    
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -18,7 +20,7 @@ public class Main extends Application {
             ScrollPane scrollPane = loader.load();
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFx application");
             primaryStage.show();
@@ -29,6 +31,10 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
 }
